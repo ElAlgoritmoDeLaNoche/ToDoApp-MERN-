732c1c5d35b20548d3ac9a47faa19e0850666e75
Francisco Navarrete <francisconavarrete@MacBook-Air-de-Francisco.local>
2022-08-06 10:22:36 -0500

    ToDoApp creado con Mongo Express React y Node V1

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+// Importar conexión a db
+
+// Importación del archivo de rutas y modelo
+const routeTask = ''
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// app.use('/api/v1', routeTask)
+
+app.get('/', (req, res) => {
+  res.send('Funcionando...')
+})
+
+app.listen(5000, function () {
+  console.log('Server listening on port' + 5000)
+})
