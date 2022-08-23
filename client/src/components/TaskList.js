e70d98f679f654971359eea6c098df931625333d
Francisco Navarrete <francisconavarrete@MacBook-Air-de-Francisco.local>
2022-08-23 12:55:45 -0500

    07 Task Read

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Container, Grid } from '@nextui-org/react'
+import TaskItem from './TaskItem'
+import axios from 'axios'
 
 const TaskList = () => {
+
+  const [dataTask, setDataTask] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/v1/tasks')
+      .then((res) => {
+        setDataTask(res.data)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
+  const tasks = dataTask.map((task) => {
+    return (
+      <TaskItem task={task} />
+    )
+  })
+
   return (
-    <div>TaskList</div>
+    <div>
+      <Container>
+        <h3>Task List</h3>
+        <Grid.Container gap={2}>
+          {tasks}
+        </Grid.Container>
+      </Container>
+    </div>
   )
 }
 
